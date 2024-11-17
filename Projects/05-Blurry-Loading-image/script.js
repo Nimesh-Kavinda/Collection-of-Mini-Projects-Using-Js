f2cef0fb2662fc02b082aeb77a0a8bcd911f5b4a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-17 14:27:39 +0530

    Finish project bulrry loading

---
+++ Projects/05-Blurry-Loading-image/script.js
@@ -0,0 +1,25 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(bulurring, 30);   
+
+function bulurring(){
+    load ++;
+
+
+    if(load > 99){
+        clearInterval(int); 
+    }
+
+    loadText.innerText = `${load}%`; 
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, inMin, inMax, outMin, outMax) => {
+return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
+}
+
