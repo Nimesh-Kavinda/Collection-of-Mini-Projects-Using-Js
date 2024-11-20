e7ff1e31de0333c964b52427ace2aa415e1f030f
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-20 19:58:00 +0530

    Covert all Script to Async-Await method and Finish Project Jokes

--- Projects/10-Jokes/script.js
+++ Projects/10-Jokes/script.js
@@ -5,7 +5,10 @@ jokeBtn.addEventListener("click", genarteJoke);
 
 genarteJoke();
 
-function genarteJoke() {
+
+// Using ASYNC/AWAIT 
+
+ async function genarteJoke() {
 
     const config = {
         headers: {
@@ -13,10 +16,35 @@ function genarteJoke() {
         },
     }
 
-    fetch('https://icanhazdadjoke.com/' , config)
-        .then((response) => response.json())
-        .then((data) => {
-            jokeEl.innerText = data.joke;
-        });
+   const res = await fetch('https://icanhazdadjoke.com/' , config);
+      
     
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
+
+
 }
+
+
+
+
+// Using .then 
+
+
+
+// function genarteJoke() {
+
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com/' , config)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             jokeEl.innerText = data.joke;
+//         });
+    
+// }
