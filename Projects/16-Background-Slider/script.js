4a17db9655d5f3de851467ab18134cc6d361b831
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-23 13:11:30 +0530

    Finish the Project Background Sliding

--- Projects/16-Background-Slider/script.js
+++ Projects/16-Background-Slider/script.js
@@ -1,4 +1,43 @@
 const body = document.body;
-const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll('.silde');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
+
+let activeSlide = 0;
+
+rightBtn.addEventListener("click", () => {
+    activeSlide++;
+
+    if(activeSlide > slides.length - 1) {
+        activeSlide = 0;
+    }
+
+    setBgtobody();
+    setActiveSlide();
+});
+
+leftBtn.addEventListener("click" , () => {
+    activeSlide--;
+
+    if(activeSlide < 0) {
+        activeSlide = slides.length - 1 ;  
+      }
+
+    setBgtobody();
+    setActiveSlide();
+});
+
+setBgtobody()
+
+function setBgtobody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+    });
+
+    slides[activeSlide].classList.add('active');
+}
+
