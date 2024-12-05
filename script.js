7c8dd257f5af0cc8a5cb11d6bb0dfeb00b163951
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-12-05 22:55:36 +0530

    Finish adding some basic GSAP for the practicing

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const cards = document.querySelectorAll('.card');
 gsap.timeline().from(cards, {
   y: 100,
   x: -100,
+  borderRadius: '50%',
   opacity: 0,
-  duration: 2,
-  stagger: 0.5, // Stagger applies to all cards collectively
+  duration: 1,
+  stagger: 0.5,
+  ease: 'power1.inOut',
 });
